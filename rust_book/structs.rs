744c932f2d0b9ae96f15de92ec4852bb19d9b4cc
Augs Machado <augustomachadoramos@gmail.com>
2021-03-21 20:18:42 -0300

    Create the structs file

---
+++ rust_book/structs.rs
@@ -0,0 +1,6 @@
+// A struct, or structure, is a custom data type that lets you name and package together multiple
+// related values that make up a meaningful group.
+// Like tuples, the pieces of a struct can be different types. Unlike with tuples, you’ll name each
+// piece of data so it’s clear what the values mean. As a result of these names, structs are more
+// flexible than tuples: you don’t have to rely on the order of the data to specify or access the values
+// of an instance.
