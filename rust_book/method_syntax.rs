3902956cf4493438b8ddeb55be1d19adada89ebe
Augs Machado <augustomachadoramos@gmail.com>
2021-03-30 08:18:03 -0300

    Fix comments in method_syntax

--- rust_book/method_syntax.rs
+++ rust_book/method_syntax.rs
@@ -1,6 +1,6 @@
 // Methods are similar to functions: they’re declared with the fn keyword and their name, they can have parameters and a
 // return value, and they contain some code that is run when they’re called from somewhere else. However, methods are different
-// from functions in that they’re defined within the context of a struct , and their first parameter is always self, which
+// from functions in that they’re defined within the context of a struct, and their first parameter is always self, which
 // represents the instance of the struct the method is being called on.
 
 #[derive(Debug)]
