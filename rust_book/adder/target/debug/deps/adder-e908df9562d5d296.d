66a147eeb4ac5228abc1db4f50f78311fcfb0319
Augs Machado <augustomachadoramos@gmail.com>
2021-04-09 23:27:11 -0300

    [rust_book:adder] Add lib adder to test

---
+++ rust_book/adder/target/debug/deps/adder-e908df9562d5d296.d
@@ -0,0 +1,7 @@
+/Users/augustomachado/github/rust/rust_book/adder/target/debug/deps/adder-e908df9562d5d296.rmeta: src/lib.rs
+
+/Users/augustomachado/github/rust/rust_book/adder/target/debug/deps/libadder-e908df9562d5d296.rlib: src/lib.rs
+
+/Users/augustomachado/github/rust/rust_book/adder/target/debug/deps/adder-e908df9562d5d296.d: src/lib.rs
+
+src/lib.rs:
